33e6dc8f556aa895c06a4dcf8728c704ccb5a429
PIUphil <ahnkp08@daum.net>
2021-06-21 17:06:40 +0900

    serbot_tutorial

---
+++ my_serbot.py
@@ -0,0 +1,44 @@
+from pop import LiDAR, Pilot
+
+SPEED = 50
+direction = 0
+
+lidar = LiDAR.Rplidar()
+bot = Pilot.SerBot()
+
+lidar.connect()
+lidar.startMotor()
+
+bot.setSpeed(SPEED)
+
+while True:
+    collision = True
+
+    while collision:
+        collision = False
+        vectors = lidar.getVectors()
+        
+        for v in vectors:
+            degree = v[0]    # 각도
+            distance = v[1]  # 거리
+
+            left_hand = (direction-60)%360   # 360을 넘어서지 않도록
+            right_hand = (direction+60)%360
+
+            disc = None
+            if left_hand > right_hand:      # 왼쪽이랑 오른쪽 어느쪽이 개방되어있는지
+                disc = degree >= left_hand or degree <= right_hand
+            else:
+                disc = degree >= left_hand and degree <= right_hand
+
+            if disc:
+                if distance <= 750:
+                    collision = True
+                    bot.stop()
+                    break
+        
+        if collision:
+            direction += 30
+            direction %= 360
+    
+    bot.move(direction, 50)
