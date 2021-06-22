dc8d2520e96cde070e77445ddadfd765cef640fd
PIUphil <ahnkp08@daum.net>
2021-06-22 20:55:52 +0900

    AIoT2

---
+++ thread_test.py
@@ -0,0 +1,43 @@
+import threading
+import time
+
+def su() :
+
+    for i in range(30):
+
+        print(i)
+        time.sleep(0.5)
+        k=0
+
+def test():
+    
+    print("광필이형")
+text_1 = "목소리"
+
+def userAction(text):
+    print(text)    
+    return False
+
+steam = create_conversation_stream()
+ga = GAssistant(steam, local_device_handler=userAction)
+
+def onStart():
+    print(">>> Start recording...")
+
+while True:
+    ga.assist(onStart)
+
+
+def 목소리를 텍스트로():
+    while True:
+        구글 어시스트
+        text_1=text  
+
+t = threading.Thread(target=su)
+#t.start()
+#time.sleep(3)
+#test()
+k = 0
+while True:
+    
+    
